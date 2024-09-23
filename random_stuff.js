ef21ef9890c6ff800f261071efe791593e2883a1
Lizarduck <benjaminwoods1001@gmail.com>
2024-09-22 18:46:42 -0700

    literal notation

--- random_stuff.js
+++ random_stuff.js
@@ -1,4 +1,4 @@
-let = numberOne = prompt("Pick a number");
-let numberTwo = prompt("Pick some other number");
+let = numberOne = prompt(`Pick a number`);
+let numberTwo = prompt(`Pick some other number`);
 let sum = numberOne + numberTwo;
 alert(sum);
