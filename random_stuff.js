8dd8b3da55f5f7f6a446c8b124abb385682fa287
Lizarduck <benjaminwoods1001@gmail.com>
2024-09-22 20:00:46 -0700

    made some charAt stuff

--- random_stuff.js
+++ random_stuff.js
@@ -1,4 +1,7 @@
-let = numberOne = prompt(`Pick a number`);
+let numberOne = prompt(`Pick a number`);
 let numberTwo = prompt(`Pick some other number`);
 let sum = numberOne + numberTwo;
 alert(sum);
+
+let randomLetters = "nvkjhbsekhrb";
+randomLetters.charAt(4);
