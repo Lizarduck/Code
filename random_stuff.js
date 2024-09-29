d6f4261e187abd43c465ff5dcac0f09c82688a10
Lizarduck <benjaminwoods1001@gmail.com>
2024-09-29 16:27:05 -0700

    messed around with different data types

--- random_stuff.js
+++ random_stuff.js
@@ -1,7 +1,15 @@
 let numberOne = prompt(`Pick a number`);
 let numberTwo = prompt(`Pick some other number`);
-let sum = numberOne + numberTwo;
+let sum = parseFloat(numberOne) + parseFloat(numberTwo);
 alert(sum);
 
 let randomLetters = "nvkjhbsekhrb";
 randomLetters.charAt(4);
+
+if (!city) {
+  alert("City is a required field");
+}
+
+let symbol1 = Symbol(`mysymbol`);
+let symbol2 = Symbol(`mysymbol`);
+symbol1 === symbol2;
