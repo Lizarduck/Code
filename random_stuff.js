08431acf8d6d7cfa719a13d9864e445166dec800
Lizarduck <benjaminwoods1001@gmail.com>
2024-10-10 21:25:06 -0700

    learning new stuff!

--- random_stuff.js
+++ random_stuff.js
@@ -14,12 +14,6 @@ let symbol1 = Symbol(`mysymbol`);
 let symbol2 = Symbol(`mysymbol`);
 symbol1 === symbol2;
 
-const wiggle = {
-  wiggleSpeed: `bursting`,
-  wiggleLevel: 36,
-  wiggleProofSubscriptionOn: true,
-};
-
 x = x ^ 2;
 
 Boolean(0 == `0`);
@@ -27,8 +21,31 @@ Boolean(0 == `0`);
 let userName = prompt(`What is your name?`);
 let welcomeMessage = alert(`Welcome, ` + userName);
 
-const dt = date();
-const hours = dt.getHours();
-let msg;
-msg = hours < 12 ? (`Good morning!`) ; (`Welcome`);
-console.log(msg);
+let wiggle = 23;
+if (
+  wiggle === 20 ||
+  wiggle === 21 ||
+  wiggle === 22 ||
+  wiggle === 23 ||
+  wiggle === 24 ||
+  wiggle === 25 ||
+  wiggle === 26 ||
+  wiggle === 27 ||
+  wiggle === 28 ||
+  wiggle === 29
+) {
+  bingles = "yes";
+}
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+for (let bogus = 300; bogus <= 1; bogus--) {
+  console.log(bogus);
+}
+
+const pets = [`cat`, `dog`, `chicken`];
+for (let pet of pets) {
+  console.log(pet);
+}
